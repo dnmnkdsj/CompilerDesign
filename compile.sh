9532c8fccc3dc020d36e642126644d4020ae3546
张震宇 <1928088706@qq.com>
2018-10-22 17:06:18 +0800

    finish annotion identification | renew docs

--- compile.sh
+++ compile.sh
@@ -1,3 +1,4 @@
 flex exp.l
 bison -d exp.y
 gcc -o parser lex.yy.c exp.tab.c ast.c
+./parser test.c
--- test.c
+++ test.c
@@ -2,7 +2,10 @@ int main() {
   int a = 10,b=20;
   float flt = 20;
   if (a<b){
-    int c = a+b;
+    /* this is
+    just a
+    annotation */
+    int c = a+b;// this is a single line annotation &*$#
     c = b-a;
   }
   return 0;
